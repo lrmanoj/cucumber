44a83108086a93b5290249eeaf43e38751adc5c6
lr manojkumar <lrmanoj33@gmail.com>
2021-12-15 15:12:23 +0530

    base done

---
+++ src/Cucumber1/CucumberNew1.java
@@ -0,0 +1,19 @@
+package Cucumber1;
+
+public class CucumberNew1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated metho
+	int	time = 20;
+	
+	if(time<18) {
+		System.out.println("false");
+	}else {
+			System.out.println("true");
+		
+		}
+	}
+		
+		
+
+}
